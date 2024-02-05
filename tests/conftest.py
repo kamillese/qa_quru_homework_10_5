561aa3adec0e5092d6941d5d6ab75749d5cb3e38
Kamilla Sergali <skamillese@gmail.com>
2024-02-05 23:28:11 +0100

    zoom script for browser and new elements were added

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.window_width = 1900
+    browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
 
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -6,6 +6,7 @@ def test_complete_todo():
     browser.open('/')
     if browser.element(by.text('Соглашаюсь')).matching(be.visible):
         browser.element(by.text('Соглашаюсь')).click()
+    browser.driver.execute_script("document.querySelector('.body-height').style.transform='scale(.50)'")
     browser.element('#firstName').should(be.blank).type('Zarina')
     browser.element('#lastName').should(be.blank).type('Ansar')
     browser.element('#userEmail').should(be.blank).type('zari12@gmail.com')
@@ -16,5 +17,26 @@ def test_complete_todo():
     browser.element('.react-datepicker__year-select').element('[value="1999"]').click()
     browser.element('.react-datepicker__day--005').click()
     browser.element('#subjectsInput').should(be.blank).type('Physics').press_enter()
-    browser.element('#subjectsInput').type('Maths').press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture.form-control-file').send_keys(os.path.abspath('2.jpg'))
+    browser.element('#currentAddress').should(be.blank).type('Via Nemorense, 100')
+    browser.element('#state').should(be.clickable).click()
+    browser.element(by.text('Uttar Pradesh')).should(be.clickable).click()
+    browser.element('#city').should(be.clickable).click()
+    browser.element(by.text('Lucknow')).should(be.clickable).click()
+    browser.element('#submit').click()
+
+    browser.element('.table-responsive').all('td:nth-child(2)').should(have.texts(
+        'Zarina Ansar',
+        'zari12@gmail.com',
+        'Female',
+        '7778234566',
+        '05 June,1999',
+        'Physics',
+        'Sports',
+        '2.jpg',
+        'Via Nemorense, 100',
+        'Uttar Pradesh Lucknow'
+    ))
+
+
